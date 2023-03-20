ae04d996f97ec6653d848bd03645f98c5d6f9160
Tushar Gupta <kshitizgupta108@gmail.com>
2023-03-20 09:14:30 +0530

    made the file structure

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var AdminContract = artifacts.require("./AdminContract.sol");
-var NodeContract = artifacts.require("./NodeContract.sol");
-
-module.exports = async function(deployer) {
-    await deployer.deploy(AdminContract);
-    await deployer.deploy(NodeContract);
-};
